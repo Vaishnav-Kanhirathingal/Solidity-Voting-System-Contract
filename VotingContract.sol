d45797a3878fa7080a1272c4db05333a1d5d0749
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-03-18 17:11:58 +0530

    contract efficiency improved, ready for deployment

--- VotingContract.sol
+++ VotingContract.sol
@@ -40,50 +40,18 @@ contract VotingContract {
         }
     }
 
-    //convertor-function-from-stack
-    function uint2str(uint256 _i)
-        internal
-        pure
-        returns (string memory _uintAsString)
-    {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-
     function getAddressValues() public view returns (address[] memory) {
         return addressStorage;
     }
 
-    function getElectionDetails() public view returns (string memory) {
-        string memory party1 = string(
-            abi.encodePacked("[Party 1 Votes = ", uint2str(partyOneVotes),"]-----[")
-        );
-        string memory party2 = string(
-            abi.encodePacked("Party 2 Votes = ", uint2str(partyTwoVotes),"]-----[")
-        );
-        string memory party3 = string(
-            abi.encodePacked("Party 3 Votes = ", uint2str(partyThreeVotes),"]")
-        );
-        string memory returnable = string(
-            abi.encodePacked(party1, party2, party3)
-        );
-        return returnable;
+    function getPartyOneVotes() public view returns (uint256){
+        return partyOneVotes;
     }
+    function getPartyTwoVotes() public view returns (uint256){
+        return partyTwoVotes;
+    }
+    function getPartyThreeVotes() public view returns (uint256){
+        return partyThreeVotes;
+    }
+    
 }
